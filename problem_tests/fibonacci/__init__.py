0baf763f55b23c7ecd9b64c554588aa2eab14e66
Pavel Kasian <135467123+Shupula@users.noreply.github.com>
2024-01-28 15:38:30 +0100

    Added tests using python

---
+++ problem_tests/fibonacci/__init__.py
@@ -0,0 +1,23 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+    check50.exists("fibonacci.py")
+
+@check50.check()
+def test1():
+    """Fibonacci function correctly calculates number on 5th place"""
+    check50.run("python3 fibonacci.py").stdin("5", regex=False).stdout("3", regex=False).exit(0)
+
+@check50.check()
+def test2():
+    """Fibonacci function correctly calculates number on 5th place"""
+    check50.run("python3 fibonacci.py").stdin("2", regex=False).stdout("1", regex=False).exit(0)
+
+@check50.check()
+def test3():
+    """Fibonacci funtion accepts only numbers"""
+    actual = check50.run("python3 fibonacci.py").stdin("abc").stdout()
+    if not re.match(r"[A-Za-z]+", actual):
+        raise check50.Failure("Your function does't provide message in case of alphabetic input")
