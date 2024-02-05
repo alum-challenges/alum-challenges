066eea0bc4bc1f8016d19488b8946b63fecca6ff
Makaze <christopherslane.work@gmail.com>
2024-02-05 11:57:25 -0500

    Revamp cookie banner

--- challenges/settings.py
+++ challenges/settings.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import os
+# import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
