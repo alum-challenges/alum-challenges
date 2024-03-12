090b18f1d839cadbe12224e28e4aeb84ba33952a
root <user.name>
2024-03-12 02:22:23 +0000

    Fix key

--- challenges/challenges/settings.py
+++ challenges/challenges/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "!*DV!9EaQC;.WiSY_y~:kFZrPW5cSujk^>qPZ>@Doi*i7x=,f7d_azGI][0kM~v"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
