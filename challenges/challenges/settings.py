4b09055f28f8841db5aaa2383d75736a50e32292
Pavel Kasian <135467123+Shupula@users.noreply.github.com>
2024-03-15 21:24:03 +0100

    Update settings.py
    Signed-off-by: Pavel Kasian <135467123+Shupula@users.noreply.github.com>

--- challenges/challenges/settings.py
+++ challenges/challenges/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True # TEMPORARY
 
 ALLOWED_HOSTS = ['146.190.35.207', '127.0.0.1', 'localhost']
 
