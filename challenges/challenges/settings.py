5e06025e2754f840051b1ee148b5840e72e8c765
Pavel Kasian <135467123+Shupula@users.noreply.github.com>
2024-03-17 15:25:11 +0100

    Update settings.py

--- challenges/challenges/settings.py
+++ challenges/challenges/settings.py
@@ -109,6 +109,7 @@ SOCIAL_AUTH_GITHUB_SECRET = '7481e083ecb15906e271ef1a45088bd745a737e7'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
+SECURE_PROXY_SSL_HEADER=('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
